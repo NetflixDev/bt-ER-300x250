70b9848b1310ee8394a173a42a6defc72da841e7
Derek Miranda <derek.miranda@ff0000.com>
2019-04-02 15:33:01 -0700

    refactor: putting and using default keyart in build
    - re-dist and -pub

--- dist/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ dist/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -14,7 +14,7 @@ module.exports = {
   "creative": "./3-traffic/default/300x250",
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1554165084427,
+  "updated": 1554244291734,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
