0fb79a14bf01768ee67e9971c87f2e062c7505e3
Derek Miranda <derek.miranda@ff0000.com>
2019-04-02 17:52:12 -0700

    fix: rebuild and redist

--- src/3-traffic/default/300x250/creative.js
+++ src/3-traffic/default/300x250/creative.js
@@ -1,43 +1,14 @@
 var Creative = function() {
 	this.play = function() {
 		console.log('Creative.play()')
+		var delay = 0
+		TweenLite.from(View.endFrame.keyart, 0.5, { alpha: 0, delay: delay })
+		TweenLite.from(View.endFrame.tuneIn, 0.5, { alpha: 0, delay: delay })
+		TweenLite.from(View.endFrame.ftm, 0.5, { alpha: 0, delay: delay })
+		TweenLite.from(View.endFrame.netflixLogo, 0.5, { alpha: 0, delay: delay })
+		TweenLite.from(View.endFrame.cta, 0.5, { alpha: 0, delay: delay })
 
-		TweenLite.to(View.endFrame.keyart, 2, {
-			scale: 1.1,
-			delay: 0
-		})
-
-		var topDelay = 2
-		var topAnimTime = 1
-		TweenLite.from(View.endFrame.tt, topAnimTime, {
-			alpha: 0,
-			delay: topDelay
-		})
-		TweenLite.from(View.endFrame.tuneIn, topAnimTime, {
-			alpha: 0,
-			delay: topDelay
-		})
-		TweenLite.from(View.endFrame.ftm, topAnimTime, {
-			alpha: 0,
-			delay: topDelay
-		})
-		TweenLite.from(View.endFrame.pedigree, topAnimTime, {
-			alpha: 0,
-			delay: topDelay
-		})
-
-		var bottomDelay = topDelay + topAnimTime + 0.3
-		var bottomAnimTime = 1
-		TweenLite.from(View.endFrame.netflixLogo, topAnimTime, {
-			alpha: 0,
-			delay: bottomDelay
-		})
-		TweenLite.from(View.endFrame.cta, topAnimTime, {
-			alpha: 0,
-			delay: bottomDelay
-		})
-
-		TweenLite.delayedCall(bottomDelay + bottomAnimTime, function() {
+		TweenLite.delayedCall(delay, function() {
 			View.endFrame.netflixLogo.play()
 		})
 	}
