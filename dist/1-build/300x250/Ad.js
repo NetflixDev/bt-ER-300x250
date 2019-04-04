6081abd4184d7fb5c67a181ac4d7924b4e2e79f3
Derek Miranda <derek.miranda@ff0000.com>
2019-04-04 11:47:20 -0700

    feat: using right-stacked layout

--- dist/1-build/300x250/Ad.js
+++ dist/1-build/300x250/Ad.js
@@ -24,7 +24,7 @@ export class Ad {
 		Control.preMarkup()
 
 		View.main = new Main()
-		View.endFrame = new EndFrame({ target: View.main })
+		View.endFrame = new EndFrame({ target: View.main, layout: RIGHT_STACKED })
 
 		View.mainBorder = new MainBorder()
 	}
