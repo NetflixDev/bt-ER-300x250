423acdad998637d9226830f64959b79ad9bb20aa
Derek Miranda <derek.miranda@ff0000.com>
2019-04-03 17:30:43 -0700

    refactor: removed supercut keys
    - redist, repub

--- src/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ src/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -14,7 +14,7 @@ module.exports = {
   "creative": "./3-traffic/default/300x250",
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1554319049986,
+  "updated": 1554337676830,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
