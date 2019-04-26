9a4dbc76161dd3871101c28c5cf3b51fd40429b5
Derek Miranda <derek.miranda@ff0000.com>
2019-04-25 17:12:24 -0700

    pkg: repub after committing previously ignored assets

--- dist/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ dist/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1556221580578,
+  "updated": 1556237460749,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
--- dist/3-traffic/default/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ dist/3-traffic/default/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -6,16 +6,25 @@ if (typeof module === 'undefined') {
 module.exports = {
   "id": "8c417243-849b-4868-8eb9-91063e2f3d80",
   "name": "bt-ER-300x250",
-  "label": "300x250",
   "description": "C20/Monet-compatible templates that use  https://github.com/ff0000-ad-tech/ad-docs/blob/master/README.md",
   "type": "build_templates",
-  "tier": "endframe-resolve",
-  "width": "300",
-  "height": "250",
-  "creative": "./3-traffic/default/300x250",
+  "builder": {
+    "label": "300x250 NF Global Template",
+    "tier": "endframe-resolve",
+    "size": "300x250",
+    "creative": "./3-traffic/default/300x250",
+    "layouts": {
+      "SIDE_BY_SIDE_1": "Side By Side 1",
+      "SIDE_BY_SIDE_2": "Side By Side 2",
+      "CORNER_LEFT": "Corner Left",
+      "CORNER_RIGHT": "Corner Right",
+      "ONE_LINE_1": "One Line 1",
+      "ONE_LINE_2": "One Line 2"
+    }
+  },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1556133471353,
+  "updated": 1556237460749,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
