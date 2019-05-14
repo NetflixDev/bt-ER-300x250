3620c1fe2fda01893a201058e2f073b99b81a0f8
Greg Connell <greg.connell@ff0000.com>
2019-05-14 15:09:47 -0700

    misc

---
+++ dist/1-build/common/js/EndFrame/inits/index.js
@@ -0,0 +1,127 @@
+import { Styles, Markup, Align, Effects } from 'ad-view'
+import { ImageManager } from 'ad-control'
+import { Animation } from '@common/js/Animation.js'
+import { Control } from '@common/js/Control.js'
+import CanvasIris from '@common/js/CanvasIris.js'
+import { UIComponent, UIBorder, UIButton, UIImage, TextFormat, UITextField, UISvg } from 'ad-ui'
+import { ObjectUtils } from 'ad-utils'
+
+export { sideBySideInit, cornerInit, oneLineInit }
+
+function sideBySideInit(T) {
+	baseInit(T, { ctaLogoInit: sideBySideCtaLogoInit })
+}
+
+function cornerInit(T) {
+	baseInit(T, { ctaLogoInit: cornerCtaLogoInit })
+}
+
+function oneLineInit(T) {
+	baseInit(T, { ctaLogoInit: oneLineCtaLogoInit })
+}
+
+function baseInit(T, { ctaLogoInit = sideBySideCtaLogoInit }) {
+	T.background = document.createElement('netflix-img')
+	T.background.setAttribute('data-dynamic-key', 'Background')
+	T.background.setAttribute('width', adParams.adWidth)
+	T.appendChild(T.background)
+
+	T.pedigree = new UITextField({
+		target: T,
+		id: 'pedigree',
+		css: {
+			width: 200,
+			height: 50
+		},
+		fontSize: 16,
+		fontFamily: 'Netflix Sans',
+		format: TextFormat.INLINE_FIT_CLAMP,
+		alignText: Align.CENTER,
+		spacing: -0.2,
+		text: ''
+	})
+
+	// title treatment
+	T.tt = document.createElement('netflix-img')
+	T.tt.setAttribute('data-dynamic-key', 'Title_Treatment')
+	T.appendChild(T.tt)
+
+	// free trial messaging
+	T.ftm = document.createElement('netflix-text')
+	T.ftm.setAttribute('data-dynamic-key', 'FTM')
+	T.appendChild(T.ftm)
+
+	// tune-in
+	T.tuneIn = document.createElement('netflix-text')
+	T.tuneIn.setAttribute('data-dynamic-key', 'Tune_In')
+	T.appendChild(T.tuneIn)
+
+	// init CTA and logo
+	ctaLogoInit(T)
+
+	// ratings bug
+	T.ratingsBug = document.createElement('netflix-img')
+	T.ratingsBug.setAttribute('data-dynamic-key', 'Ratings_Bug_20x20')
+	T.ratingsBug.setAttribute('id', 'ratings_bug')
+	T.ratingsBug.setAttribute('width', 20)
+	T.appendChild(T.ratingsBug)
+
+	T.iris =
+		window.Creative &&
+		Creative.usesCanvasIris &&
+		new CanvasIris({
+			target: T,
+			irisColor: Creative.irisColor
+		})
+}
+
+function sideBySideCtaLogoInit(T) {
+	// logo
+	T.netflixLogo = document.createElement('netflix-brand-logo')
+	T.netflixLogo.setAttribute('width', 90)
+	T.appendChild(T.netflixLogo)
+
+	// cta
+	T.cta = document.createElement('netflix-cta')
+	T.cta.setAttribute('data-dynamic-key', 'CTA')
+	T.cta.setAttribute('arrow', '')
+	T.cta.setAttribute('border', '')
+	T.cta.setAttribute('width', 90)
+	T.cta.setAttribute('height', 24)
+	T.appendChild(T.cta)
+}
+
+function cornerCtaLogoInit(T) {
+	// logo
+	T.netflixLogo = document.createElement('netflix-brand-logo')
+	T.netflixLogo.setAttribute('width', 75)
+	T.appendChild(T.netflixLogo)
+
+	// cta
+	T.cta = document.createElement('netflix-cta')
+	T.cta.setAttribute('data-dynamic-key', 'CTA')
+	T.cta.setAttribute('arrow', '')
+	T.cta.setAttribute('border', '')
+	T.cta.setAttribute('width', 75)
+	T.cta.setAttribute('height', 20)
+	T.cta.setAttribute('min-font-size', 7)
+	T.cta.setAttribute('stretch-origin', 'right')
+	T.appendChild(T.cta)
+}
+
+function oneLineCtaLogoInit(T) {
+	// logo
+	T.netflixLogo = document.createElement('netflix-brand-logo')
+	T.netflixLogo.setAttribute('width', 75)
+	T.appendChild(T.netflixLogo)
+
+	// cta
+	T.cta = document.createElement('netflix-cta')
+	T.cta.setAttribute('data-dynamic-key', 'CTA')
+	T.cta.setAttribute('arrow', '')
+	T.cta.setAttribute('border', '')
+	T.cta.setAttribute('width', 83)
+	T.cta.setAttribute('max-width', 90)
+	T.cta.setAttribute('height', 22)
+	T.appendChild(T.cta)
+}
---
+++ dist/1-build/common/js/EndFrame/postmarkups/index.js
@@ -0,0 +1,6 @@
+import sideBySidePostMarkup from './sideBySidePostMarkup.js'
+import cornerLeftPostMarkup from './cornerLeftPostMarkup.js'
+import cornerRightPostMarkup from './cornerRightPostMarkup.js'
+import oneLinePostMarkup from './oneLinePostMarkup.js'
+
+export { sideBySidePostMarkup, cornerLeftPostMarkup, cornerRightPostMarkup, oneLinePostMarkup }
