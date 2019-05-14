59cd40a63088f2d0d070ab5d1e79fb3934ccecea
Greg Connell <greg.connell@ff0000.com>
2019-05-14 14:48:29 -0700

    improving tooltip text

--- dist/1-build/common/js/EndFrame/postmarkups/index.js
+++
@@ -1,6 +0,0 @@
-import sideBySidePostMarkup from './sideBySidePostMarkup.js'
-import cornerLeftPostMarkup from './cornerLeftPostMarkup.js'
-import cornerRightPostMarkup from './cornerRightPostMarkup.js'
-import oneLinePostMarkup from './oneLinePostMarkup.js'
-
-export { sideBySidePostMarkup, cornerLeftPostMarkup, cornerRightPostMarkup, oneLinePostMarkup }
