bd0829f27c61ccad0ab1b1a41c610e4ec7950533
Greg Connell <greg.connell@ff0000.com>
2019-05-13 18:06:54 -0700

    adds container-level manifest data

--- dist/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ dist/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1557795221266,
+  "updated": 1557795989470,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
