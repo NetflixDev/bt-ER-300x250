6bf73d6f2b3b62929f35e7ecdecfbdc57e1c76c0
Derek Miranda <derek.miranda@ff0000.com>
2019-06-27 17:54:06 -0700

    docs: supercut requirements added to tooltip

--- dist/3-traffic/default/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ dist/3-traffic/default/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1561496533634,
+  "updated": 1561683230534,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
