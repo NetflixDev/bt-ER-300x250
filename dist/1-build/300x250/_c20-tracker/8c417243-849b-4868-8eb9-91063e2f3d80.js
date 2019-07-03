e8a94eef09a88c675ea95c6788e2e83fe4d9ede7
Derek Miranda <derek.miranda@ff0000.com>
2019-07-03 14:20:13 -0700

    refactor: puts default C2.0 bg in treatment-assets
    - redist

--- dist/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ dist/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1562093494936,
+  "updated": 1562188794301,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
