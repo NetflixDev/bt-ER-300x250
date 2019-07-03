77811c2b44590b4d0d6e5569250a239656523afc
Derek Miranda <derek.miranda@ff0000.com>
2019-07-02 17:25:57 -0700

    feature: new animation order
    - show endframe > ribbon > supercut > then endframe again
    - ribbon no matter what
    - treatment assets in own folder

--- src/1-build/common/js/Animation.js
+++ src/1-build/common/js/Animation.js
@@ -1,48 +1,69 @@
-import { Styles, Markup, Align, Effects } from 'ad-view'
+import { Styles, Markup, Align, Effects } from "ad-view";
+
+const INIT_ZOOM_START = 1;
+const INIT_ZOOM_SCALE = 1.5;
+const KEYART_HOLD_START = INIT_ZOOM_START + 0.5;
 
 export class Animation {
-	static start() {
-		console.log('Animation.start()')
-		// show the main container
-		global.removePreloader()
-		Styles.setCss(View.main, { opacity: 1 })
+  static start() {
+    console.log("Animation.start()");
+    // show the main container
+    global.removePreloader();
+    Styles.setCss(View.main, { opacity: 1 });
 
-		if (View.ribbon) {
-			View.ribbon.play()
-		} else {
-			Animation.playIntro()
-		}
-	}
+    if (adData.useSupercut) {
+      View.endFrame.show();
+      // have Netflix logo already fully in
+      View.endFrame.netflixLogo.progress(1);
 
-	// IMPORTANT!!! Call this function when your animation is complete!
-	static complete() {
-		console.log('Animation.complete()')
-	}
+      TweenLite.to(View.endFrame, 1, {
+        scale: INIT_ZOOM_SCALE,
+        delay: INIT_ZOOM_START
+      });
 
-	static playIntro() {
-		if (View.intro) {
-			Styles.setCss(View.intro.netflixLogo, { opacity: 1 })
-			View.intro.introVideoPlayer.play()
+      TweenLite.delayedCall(KEYART_HOLD_START, () => {
+        View.ribbon.play();
+      });
+    } else {
+      Animation.playIntro();
+    }
+  }
 
-			TweenLite.delayedCall(2.5, function() {
-				View.intro.netflixLogo.reverse()
-			})
-			TweenLite.delayedCall(6, function() {
-				View.intro.netflixLogo.play()
-			})
-			TweenLite.to(View.intro.netflixLogo, 0.25, { delay: 8, alpha: 0 })
-		} else {
-			Animation.showEndFrame()
-		}
-	}
+  // IMPORTANT!!! Call this function when your animation is complete!
+  static complete() {
+    console.log("Animation.complete()");
+  }
 
-	static showEndFrame() {
-		console.log('Animation.showEndFrame()')
+  static playIntro() {
+    if (View.intro) {
+      Styles.setCss(View.intro.netflixLogo, { opacity: 1 });
+      View.intro.introVideoPlayer.play();
 
-		if (View.intro) View.intro.hide()
-		View.endFrame.show()
+      TweenLite.delayedCall(2.5, function() {
+        View.intro.netflixLogo.reverse();
+      });
+      TweenLite.delayedCall(6, function() {
+        View.intro.netflixLogo.play();
+      });
+      TweenLite.to(View.intro.netflixLogo, 0.25, { delay: 8, alpha: 0 });
+    } else {
+      Animation.showEndFrame();
+    }
+  }
 
-		const creative = new Creative()
-		creative.play()
-	}
+  static showEndFrame() {
+    console.log("Animation.showEndFrame()");
+
+    if (adData.useSupercut) {
+      // reset endframe after ribbon and supercut
+      View.endFrame.netflixLogo.progress(0);
+      TweenLite.set(View.endFrame, { scale: 1 });
+    }
+
+    if (View.intro) View.intro.hide();
+    View.endFrame.show();
+
+    const creative = new Creative();
+    creative.play();
+  }
 }
