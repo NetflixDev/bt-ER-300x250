6f9b1e760362deff9fa1aeaa1555f1405d3e601a
Derek Miranda <derek.miranda@ff0000.com>
2019-07-10 17:32:16 -0700

    fix: turns iris canvas off on start

--- src/1-build/common/js/Animation.js
+++ src/1-build/common/js/Animation.js
@@ -1,69 +1,80 @@
-import { Styles, Markup, Align, Effects } from "ad-view";
+import { Styles, Markup, Align, Effects } from 'ad-view'
 
-const INIT_ZOOM_START = 1;
-const INIT_ZOOM_SCALE = 1.5;
-const KEYART_HOLD_START = INIT_ZOOM_START + 0.5;
+const RIBBON_ANIM_TIME = 0.6
+const INIT_ZOOM_START = 1.5
+const INIT_ZOOM_SCALE = 2
+const INIT_ZOOM_DURATION = RIBBON_ANIM_TIME + 1
+const RIBBON_START = INIT_ZOOM_START + INIT_ZOOM_DURATION - 0.75
 
 export class Animation {
-  static start() {
-    console.log("Animation.start()");
-    // show the main container
-    global.removePreloader();
-    Styles.setCss(View.main, { opacity: 1 });
+	static start() {
+		console.log('Animation.start()')
 
-    if (adData.useSupercut) {
-      View.endFrame.show();
-      // have Netflix logo already fully in
-      View.endFrame.netflixLogo.progress(1);
+		if (View.endFrame.iris) {
+			TweenLite.set(View.endFrame.iris.canvas, { opacity: 0 })
+		}
 
-      TweenLite.to(View.endFrame, 1, {
-        scale: INIT_ZOOM_SCALE,
-        delay: INIT_ZOOM_START
-      });
+		// show the main container
+		global.removePreloader()
+		Styles.setCss(View.main, { opacity: 1 })
 
-      TweenLite.delayedCall(KEYART_HOLD_START, () => {
-        View.ribbon.play();
-      });
-    } else {
-      Animation.playIntro();
-    }
-  }
+		if (adData.useSupercut) {
+			View.endFrame.show()
+			// have Netflix logo already fully in
+			View.endFrame.netflixLogo.progress(1)
 
-  // IMPORTANT!!! Call this function when your animation is complete!
-  static complete() {
-    console.log("Animation.complete()");
-  }
+			TweenLite.to(View.endFrame, INIT_ZOOM_DURATION, {
+				delay: INIT_ZOOM_START,
+				scale: INIT_ZOOM_SCALE,
+				ease: Power2.easeIn
+			})
 
-  static playIntro() {
-    if (View.intro) {
-      Styles.setCss(View.intro.netflixLogo, { opacity: 1 });
-      View.intro.introVideoPlayer.play();
+			TweenLite.delayedCall(RIBBON_START, () => {
+				View.ribbon.play()
+			})
+		} else {
+			Animation.playIntro()
+		}
+	}
 
-      TweenLite.delayedCall(2.5, function() {
-        View.intro.netflixLogo.reverse();
-      });
-      TweenLite.delayedCall(6, function() {
-        View.intro.netflixLogo.play();
-      });
-      TweenLite.to(View.intro.netflixLogo, 0.25, { delay: 8, alpha: 0 });
-    } else {
-      Animation.showEndFrame();
-    }
-  }
+	// IMPORTANT!!! Call this function when your animation is complete!
+	static complete() {
+		console.log('Animation.complete()')
+	}
 
-  static showEndFrame() {
-    console.log("Animation.showEndFrame()");
+	static playIntro() {
+		if (View.intro) {
+			Styles.setCss(View.intro.netflixLogo, { opacity: 1 })
+			View.intro.introVideoPlayer.play()
 
-    if (adData.useSupercut) {
-      // reset endframe after ribbon and supercut
-      View.endFrame.netflixLogo.progress(0);
-      TweenLite.set(View.endFrame, { scale: 1 });
-    }
+			TweenLite.delayedCall(2.5, function() {
+				View.intro.netflixLogo.reverse()
+			})
+			TweenLite.delayedCall(6, function() {
+				View.intro.netflixLogo.play()
+			})
+			TweenLite.to(View.intro.netflixLogo, 0.25, { delay: 8, alpha: 0 })
+		} else {
+			Animation.showEndFrame()
+		}
+	}
 
-    if (View.intro) View.intro.hide();
-    View.endFrame.show();
+	static showEndFrame() {
+		console.log('Animation.showEndFrame()')
 
-    const creative = new Creative();
-    creative.play();
-  }
+		if (adData.useSupercut) {
+			// reset endframe after ribbon and supercut
+			View.endFrame.netflixLogo.progress(0)
+			if (View.endFrame.iris) {
+				TweenLite.set(View.endFrame.iris.canvas, { opacity: 1 })
+			}
+			TweenLite.set(View.endFrame, { scale: 1 })
+		}
+
+		if (View.intro) View.intro.hide()
+		View.endFrame.show()
+
+		const creative = new Creative()
+		creative.play()
+	}
 }
