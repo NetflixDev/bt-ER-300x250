2e40dbaa651476c88847abfc721f57d2757cc5ab
Derek Miranda <derek.miranda@ff0000.com>
2019-07-12 13:53:21 -0700

    misc: dist/pub updates

--- src/1-build/300x250/creative.js
+++ src/1-build/300x250/creative.js
@@ -1,15 +1,29 @@
+/**
+ * This animation preset uses ff0000-ad-tech/ad-canvas package to animate canvas-rendered elements
+ * See here for more details: https://github.com/ff0000-ad-tech/ad-canvas
+ */
 var Creative = function() {
 	this.play = function() {
 		console.log('Creative.play()')
-		var delay = 0
-		TweenLite.from(View.endFrame.background, 1.5, { alpha: 0, delay: delay })
-		TweenLite.from(View.endFrame.tuneIn, 1.5, { alpha: 0, delay: delay })
-		TweenLite.from(View.endFrame.ftm, 1.5, { alpha: 0, delay: delay })
-		TweenLite.from(View.endFrame.netflixLogo, 1.5, { alpha: 0, delay: delay })
-		TweenLite.from(View.endFrame.cta, 1.5, { alpha: 0, delay: delay })
 
-		TweenLite.delayedCall(delay, function() {
+		var irisDelay = 0.5
+		var irisTime = 3.0
+		var irisLen = Math.max(adParams.adWidth, adParams.adHeight)
+		View.endFrame.iris.tween.to(View.endFrame.iris.circle, irisTime, {
+			delay: irisDelay,
+			scale: irisLen * 0.05,
+			ease: Power2.easeOut
+		})
+		View.endFrame.iris.tween.start()
+
+		var logoDelay = irisDelay + Creative.irisOffscreenAnimPercent * irisTime
+		TweenLite.delayedCall(logoDelay, function() {
 			View.endFrame.netflixLogo.play()
 		})
 	}
 }
+
+// percentage of iris animation that has to elapse before content underneath is completely visible
+Creative.irisOffscreenAnimPercent = 0.3
+Creative.usesCanvasIris = true
+Creative.irisColor = 'white'
