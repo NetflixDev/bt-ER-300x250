d3aa8f661bdcd0ccf953ce01a1cdf39d639c270d
Derek Miranda <derek.miranda@ff0000.com>
2019-08-08 14:56:58 -0700

    feature: RTL toggle
        - adds Toggle_RTL
        - applies RTL to CTA text

--- src/3-traffic/default/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ src/3-traffic/default/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1565128401317,
+  "updated": 1565301374264,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
