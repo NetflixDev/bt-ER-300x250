76102d1dd84e338cf5fc2ad06723c00e9809d4b2
Derek Miranda <derek.miranda@ff0000.com>
2019-09-03 14:28:10 -0700

    chore: C2.0 BT redist

--- dist/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ dist/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1565809262615,
+  "updated": 1567546065403,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
--- dist/3-traffic/default/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ dist/3-traffic/default/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -1,6 +1,6 @@
 // GENERIC SOURCE TRACKER: bt-Endframe-Resolve
-if (typeof module === "undefined") {
-  module = {};
+if (typeof module === 'undefined') {
+	module = {}
 }
 // prettier-ignore
 module.exports = {
@@ -22,7 +22,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1565809262615,
+  "updated": 1567546065403,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
