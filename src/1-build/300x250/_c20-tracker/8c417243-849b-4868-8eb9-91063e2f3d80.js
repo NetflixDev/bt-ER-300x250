8a89e44ac939f1d3acc886231c285680410fdec9
Derek Miranda <derek.miranda@ff0000.com>
2019-09-03 17:06:39 -0700

    Rebuild after reinstalling build node_modules

--- src/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ src/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -1,6 +1,6 @@
 // GENERIC SOURCE TRACKER: bt-Endframe-Resolve
-if (typeof module === "undefined") {
-  module = {};
+if (typeof module === 'undefined') {
+	module = {}
 }
 // prettier-ignore
 module.exports = {
@@ -9,6 +9,7 @@ module.exports = {
   "description": "C20/Monet-compatible templates that use  https://github.com/ff0000-ad-tech/ad-docs/blob/master/README.md",
   "type": "build_templates",
   "builder": {
+    "label": "300x250 NF Global Template",
     "tier": "endframe-resolve",
     "size": "300x250",
     "creative": "./dist/3-traffic/default/300x250",
@@ -22,7 +23,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1567546065403,
+  "updated": 1557969942685,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
