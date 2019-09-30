15f9ed9a5762bc86835ef3a46a266842317b5f9d
Derek Miranda <derek.miranda@ff0000.com>
2019-09-30 15:51:55 -0700

    Applies any remaining layout fixes
    - redist

--- src/1-build/common/js/EndFrame/postmarkups/index.js
+++ src/1-build/common/js/EndFrame/postmarkups/index.js
@@ -1,76 +1,71 @@
-import verticalSideBySide from "./verticalSideBySide.js";
-import verticalStacked from "./verticalStacked.js";
-import verticalOneLine from "./verticalOneLine.js";
-import { Align } from "ad-view";
+import verticalSideBySide from './verticalSideBySide.js'
+import verticalStacked from './verticalStacked.js'
+import verticalOneLine from './verticalOneLine.js'
+import { Align } from 'ad-view'
 
-export {
-  sideBySidePostMarkup,
-  cornerLeftPostMarkup,
-  cornerRightPostMarkup,
-  oneLinePostMarkup
-};
+export { sideBySidePostMarkup, cornerLeftPostMarkup, cornerRightPostMarkup, oneLinePostMarkup }
 
 function sideBySidePostMarkup() {
-  verticalSideBySide({
-    ctaLogoOffset: 13,
-    headlineFontSize: 16,
-    headlineLockupOffset: 15,
-    brandingLockupAlign: {
-      x: Align.CENTER,
-      y: {
-        type: Align.BOTTOM,
-        offset: -15
-      }
-    }
-  });
+	verticalSideBySide({
+		ctaLogoOffset: 13,
+		headlineFontSize: 16,
+		headlineLockupOffset: 15,
+		brandingLockupAlign: {
+			x: Align.CENTER,
+			y: {
+				type: Align.BOTTOM,
+				offset: -15
+			}
+		}
+	})
 }
 
 function cornerLeftPostMarkup() {
-  verticalStacked({
-    headlineFontSize: 14,
-    brandingLockupOffset: 11,
-    brandingLockupElemXAlign: Align.CENTER,
-    brandingLockupAlign: {
-      x: {
-        type: Align.LEFT,
-        offset: 16
-      },
-      y: {
-        type: Align.BOTTOM,
-        offset: -16
-      }
-    }
-  });
+	verticalStacked({
+		headlineFontSize: 14,
+		brandingLockupOffset: 11,
+		brandingLockupElemXAlign: Align.CENTER,
+		brandingLockupAlign: {
+			x: {
+				type: Align.LEFT,
+				offset: 16
+			},
+			y: {
+				type: Align.BOTTOM,
+				offset: -16
+			}
+		}
+	})
 }
 
 function cornerRightPostMarkup() {
-  verticalStacked({
-    headlineFontSize: 14,
-    brandingLockupOffset: 11,
-    brandingLockupElemXAlign: Align.CENTER,
-    brandingLockupAlign: {
-      x: {
-        type: Align.RIGHT,
-        offset: -16
-      },
-      y: {
-        type: Align.BOTTOM,
-        offset: -16
-      }
-    }
-  });
+	verticalStacked({
+		headlineFontSize: 14,
+		brandingLockupOffset: 11,
+		brandingLockupElemXAlign: Align.CENTER,
+		brandingLockupAlign: {
+			x: {
+				type: Align.RIGHT,
+				offset: -16
+			},
+			y: {
+				type: Align.BOTTOM,
+				offset: -16
+			}
+		}
+	})
 }
 
 function oneLinePostMarkup() {
-  verticalOneLine({
-    brandingLockupOffset: 11,
-    headlineFontSize: 15,
-    brandingLockupAlign: {
-      x: Align.CENTER,
-      y: {
-        type: Align.TOP,
-        against: 210
-      }
-    }
-  });
+	verticalOneLine({
+		brandingLockupOffset: 11,
+		headlineFontSize: 15,
+		brandingLockupAlign: {
+			x: Align.CENTER,
+			y: {
+				type: Align.TOP,
+				against: 210
+			}
+		}
+	})
 }
